1480b8662e19c3c37dbea39bcc21468788955af0
Vladimir <doroninva@gmail.com>
2025-12-06 23:45:51 +0300

    Упражнение

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello Vladimir");
+const tarif = 80;
+const hoursInDay = 5;
+const daysInWeek = 5;
+
+const timeWork = 40;
+let days = timeWork / hoursInDay + 2;
+let salary = tarif * timeWork;
+let b = (days <= 11);
+
+console.log(b);
+console.log('Количество дней работы: ' + days);
+console.log('Зарплата за работу: ' + salary);
