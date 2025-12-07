37032f54b4826b5c6576ac60e0c9917c89685bd4
Vladimir <doroninva@gmail.com>
2025-12-07 13:53:52 +0300

    Тернарный оператор

--- app.js
+++ app.js
@@ -1,17 +1,52 @@
-const depozit = 12000;
-const procentInMonth = 7;
-const months = 10;
-const costHouse = 13500;
+let role = "admin";
 
-const Amount = depozit * (1 + (procentInMonth / 100) / 12) ** months;  
-console.log(`Сумма на вашем счету через ${months} месяцев составит ${Amount} рублей.`);
-const profit = Amount - depozit; 
-console.log(`Ваш прибыль за ${months} месяцев составит ${profit} рублей.`);
 
-if (Amount >= costHouse) {
-    console.log(`Поздравляем! Вы сможете купить дом стоимостью ${costHouse} рублей.
-и у вас останется ${Amount - costHouse} рублей.`);
+if (role === "admin") {
+    console.log(`Привет ${role}! Добро пожаловать на панель управления.`);
+} else if (role === "user") {
+    console.log(`Привет ${role}! Рад видеть тебя снова.`);
+} else if (role === "guest") {
+    console.log(`Привет ${role}! Пожалуйста, зарегистрируйся.`);
 } else {
-    const difference = costHouse - Amount;
-    console.log(`К сожалению, вам не хватает ${difference} рублей для покупки дома стоимостью ${costHouse} рублей.`);
+    console.log("Привет! Ну и кто ты такой?");
 }
+
+role = "CEO";
+
+switch (role) {
+    case "admin":
+    case "administrator":
+        console.log(`Привет ${role}! Добро пожаловать на панель управления.`);
+        break
+    case "CEO":
+    case "CTO":
+        console.log(`Привет ${role}! Рад видеть тебя в руководстве.`);
+        break
+    case "user":
+        console.log(`Привет ${role}! Рад видеть тебя снова.`);
+        break
+    case "guest":
+        console.log(`Привет ${role}! Пожалуйста, зарегистрируйся.`);
+        break
+    default:
+        console.log("Привет! Ну и кто ты такой?");
+}
+
+const bmvX3 = 100000;
+const audiA4 = 80000;
+const budget = 20000;
+
+//let message = "";
+// if (bmvX3 <= budget) {
+//     message = "Вы можете позволить себе BMW X3";
+// } else if (audiA4 <= budget) {
+//     message = "Вы можете позволить себе Audi A4";
+// } else {
+//     message = "У вас недостаточно средств на покупку автомобиля";
+// }
+
+//let message = "";
+let message = bmvX3 <= budget ?  "Вы можете позволить себе BMW X3" : "У вас недостаточно средств на покупку автомобиля";
+
+
+console.log(message);
