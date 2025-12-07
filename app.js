2fc22e7079d3f825613a676acc05b74d73a60939
Vladimir <doroninva@gmail.com>
2025-12-07 17:12:52 +0300

    Упражнение case

--- app.js
+++ app.js
@@ -1,52 +1,23 @@
-let role = "admin";
+const ans = prompt('Сколько буде 7 +  или - 15?');
 
-
-if (role === "admin") {
-    console.log(`Привет ${role}! Добро пожаловать на панель управления.`);
-} else if (role === "user") {
-    console.log(`Привет ${role}! Рад видеть тебя снова.`);
-} else if (role === "guest") {
-    console.log(`Привет ${role}! Пожалуйста, зарегистрируйся.`);
-} else {
-    console.log("Привет! Ну и кто ты такой?");
-}
-
-role = "CEO";
-
-switch (role) {
-    case "admin":
-    case "administrator":
-        console.log(`Привет ${role}! Добро пожаловать на панель управления.`);
-        break
-    case "CEO":
-    case "CTO":
-        console.log(`Привет ${role}! Рад видеть тебя в руководстве.`);
-        break
-    case "user":
-        console.log(`Привет ${role}! Рад видеть тебя снова.`);
-        break
-    case "guest":
-        console.log(`Привет ${role}! Пожалуйста, зарегистрируйся.`);
-        break
+switch (ans) {
+    case '22':
+    case 'двадцать два':
+    case 'Я не робот':
+        console.log('Успех!');
+        break;
     default:
-        console.log("Привет! Ну и кто ты такой?");
+        console.log('Вы робот!');
 }
 
-const bmvX3 = 100000;
-const audiA4 = 80000;
-const budget = 20000;
 
-//let message = "";
-// if (bmvX3 <= budget) {
-//     message = "Вы можете позволить себе BMW X3";
-// } else if (audiA4 <= budget) {
-//     message = "Вы можете позволить себе Audi A4";
-// } else {
-//     message = "У вас недостаточно средств на покупку автомобиля";
-// }
-
-//let message = "";
-let message = bmvX3 <= budget ?  "Вы можете позволить себе BMW X3" : "У вас недостаточно средств на покупку автомобиля";
-
-
-console.log(message);
+switch (true) {
+    case Number(ans) === 22:
+    case Number(ans) === -8:
+    case ans === 'Я не робот':
+    case ans === 'двадцать два':
+        console.log('Успех!');
+        break;
+    default:
+        console.log('Вы робот!');
+}
