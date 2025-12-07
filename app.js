fc5515f83d7de9cab72ce66aca68a1092c27c316
Vladimir <doroninva@gmail.com>
2025-12-07 22:58:21 +0300

    Конец Булева логика

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
-let userName;
-console.log(userName || "DefaultUserName");
+const balance = 900;
+const isBanned = false;
+const bonusBalance = 90;
+const isExist = false;
+
+const isSalling = true;
+
+let isSalled =
+  (balance > 1000 || bonusBalance >= 100) && !isBanned && !isExist && isSalling;
+
+console.log(`Может купить: ${isSalled ? "Да" : "Нет"}`);
