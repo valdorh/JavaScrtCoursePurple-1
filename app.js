1e6c66fd681433311133d1aad2272c2682db417d
Vladimir <doroninva@gmail.com>
2025-12-08 11:17:12 +0300

    Окончание Функций

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
-console.log(powerOfTwo(5));
+// const KG_IN_RUB = 7;
+// const KM_IN_RUB = 5;
 
-function powerOfTwo(num) {
-  return num * num;
+// function calculateWeightInRub(weightInKg) {
+//   return weightInKg * KG_IN_RUB;
+// }
+
+// function calculateDistanceInRub(distancetInKm) {
+//   return distancetInKm * KM_IN_RUB;
+// }
+
+// function getExchangePrice(present1, present2, distance) {
+//   const weightPrice = calculateWeightInRub(present1 + present2);
+//   const distancePrice = calculateDistanceInRub(distance);
+//   return weightPrice + distancePrice;
+// }
+
+// console.log(getExchangePrice(1, 2, 10)); // Example usage
+
+const userAge = 25;
+const userJob = false;
+const userMoney = 1500;
+
+function creditOffer(age, job) {
+  if (age >= 23 && job) {
+    return 500;
+  } else if (age >= 23 && !job) {
+    return 100;
+  } else {
+    return 0;
+  }
 }
 
-const powft = (num) => num * num;
-const powft2 = (num) => num * num;
+const canBayMacbook = (money, price, age, job = false) =>
+  money + creditOffer(age, job) >= price;
 
-console.log(powft(6));
-console.log(powft2(7));
-
-const powft3 = (num, power) => num ** power;
-console.log(powft3(3, 3));
+console.log(canBayMacbook(userMoney, 2000, userAge, userJob)); // Example usage
