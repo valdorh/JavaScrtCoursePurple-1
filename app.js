4955b7a4b905935f6ed1b9ded7ffcc776a386762
Vladimir <doroninva@gmail.com>
2025-12-09 14:18:38 +0300

    Конец циклы

--- app.js
+++ app.js
@@ -1,18 +1,73 @@
-// const userData = ["Владимир", 20, "Москва"];
-// console.log(userData);
+// for (let i = 0; i < 10; i++) {
+//   console.log(`Наш баланс: ${i} $`);
+// }
+const tasks = ["Задача 1", "Задача 2", "Задача 3", "Задача 4", "Задача 5"];
 
-// const [name, _, city] = userData;
-// console.log(name, city);
+// for (let i = 0; i < tasks.length; i++) {
+//   if (tasks[i] === "Задача 3") {
+//     continue;
+//   }
+//   console.log(tasks[i]);
+// }
 
-// const data = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-// const [first, second, ...rest] = data;
-// console.log(first, second, rest);
+// console.log("----");
 
-const url = "https://example.com/api/users/12345/details";
+// for (let i = 0; i < tasks.length; i++) {
+//   if (tasks[i] === "Задача 3") {
+//     break;
+//   }
+//   console.log(tasks[i]);
+// }
 
-function parseUrl(url) {
-  const [protocol, _, domain, ...rest] = url.split("/");
-  return [protocol.replace(":", ""), domain, "/" + rest.join("/")];
+// const arr = ["!", "JS", "люблю", "Я"];
+// let str = "";
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   str += arr[i] + " ";
+// }
+// console.log(str);
+
+// for (let i = 1; i < 4; i++) {
+//   console.log(`Цикл-1: ${i} $`);
+//   for (let j = 1; j < 5; j++) {
+//     console.log(`   Цикл-2: ${j} $`);
+//   }
+// }
+
+// for (let element of tasks) {
+//   console.log(element);
+// }
+
+const operations = [800, -500, 100, -300, 10000];
+const balance = 100;
+
+function calculateBalance(beginBalance, operations) {
+  let balance = beginBalance;
+  for (let operation of operations) {
+    balance += operation;
+    negativeBalance(balance);
+  }
+  return balance;
 }
-const parsedData = parseUrl(url);
-console.log(parsedData);
+
+function negativeBalance(balance) {
+  if (balance < 0) {
+    console.log("Внимание! Ваш баланс отрицательный!");
+  }
+}
+
+function averageDebit(operat) {
+  let sum = 0;
+  let count = 0;
+  for (let operation of operat) {
+    if (operation > 0) {
+      sum += operation;
+      count++;
+    }
+  }
+  return sum / count;
+}
+
+console.log(`Начальный баланс: ${balance} $`);
+
+console.log(`Наш баланс: ${calculateBalance(balance, operations)} $`);
+console.log(`Средний доход: ${averageDebit(operations)} `);
