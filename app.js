168309b279f0bd543ff35d9b8a20c52051c5518e
Vladimir <doroninva@gmail.com>
2025-12-09 23:41:05 +0300

    Работа со строками

--- app.js
+++ app.js
@@ -1,35 +1,36 @@
-// function add(a, b) {
-//   return a + b;
-// }
+const userName = "Вася Пупкина";
+// console.log(userName[0]);
+// console.log(userName.charAt(0));
+// console.log(userName[userName.length - 1]);
+// console.log(userName.charAt(userName.length - 1));
+// console.log(userName.slice(0, 3));
+// console.log(userName.substring(0, 3));
+// console.log(userName.substr(0, 3));
 
-// function subtract(a, b) {
-//   return a - b;
-// }
+// console.log(userName.includes("а"));
+// console.log(userName.startsWith("Ва"));
+// console.log(userName.endsWith("ин"));
 
-// //Функция высшего порядка
+// console.log(userName.toUpperCase());
+// console.log(userName.toLowerCase());
+// console.log(userName.replace("Вася", "Петя"));
+// console.log(userName.replaceAll("а", "y"));
 
-// function calculate(a, b, operation) {
-//   console.log(operation.name);
-//   const result = operation(a, b);
-//   return result;
-// }
+// const nun1 = "89102233445";
+// // console.log(nun1.replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, '+7 ($1) $2-$3-$4'));
+// const nun2 = "8910-223-34-45";
+// const num5 = "910 223 34 45";
+// console.log(
+//   num5
+//     .replace(/[\-\s]/g, "")
+//     .replace(/(\d{3})(\d{3})(\d{2})(\d{2})/, "+7 ($1) $2-$3-$4")
+// );
+// const nun3 = "+7 (910) 223-34-45";
+// const num4 = "+7 910 223 34 45";
 
-// console.log(calculate(10, 5, add)); // Output: add \n 15
-// console.log(calculate(10, 5, subtract)); // Output: subtract \n 5
-// console.log(calculate(20, 8, (x, y) => x * y));
-
-function power(pow) {
-  return function (num) {
-    return num ** pow;
-  };
-}
-
-const square = power(2);
-console.log(square(5)); // Output: 25
-
-function cube(num) {
-  return (num) => num ** 3;
-}
-
-const power2 = (num) => (pow) => num ** pow;
-console.log(cube()(3)); // Output: 27
+//Упражнение корректность карты
+const cardNum = "1234 5678 9012 3456";
+const begin = cardNum.slice(0, 4);
+const end = cardNum.slice(-4);
+const maskedCardNum = begin + " **** **** " + end;
+console.log(maskedCardNum); // Output: 1234 **** **** 3456
