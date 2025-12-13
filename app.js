988280c61f39654970187ce35e886a13364e108f
Vladimir <doroninva@gmail.com>
2025-12-13 09:25:38 +0300

    Методы объектов

--- app.js
+++ app.js
@@ -1,104 +1,47 @@
-const scores = [5, 10, 8, 0, 25, 30, 15, 20, 22];
-
-// for (const [i, el] of scores.entries()) {
-//   console.log(`Раунд ${i + 1}: ${el}`);
-// }
-
-// scores.forEach((el, i) => {
-//   console.log(`Раунд ${i + 1}: ${el} очков`);
-// });
-
-//---------MAP-----------------
-
-// const scoresMap = scores.map((el, i) => {
-//   return `Раунд ${i + 1}: ${el + 0.1} очков`;
-// });
-// console.log(scoresMap);
-
-const transactioUSD = [10, -5, -15, 20, -30, 150, -40];
-const courceRUB = 81.5;
-
-// const transactioRUB = transactioUSD.map((el) => el * courceRUB);
-// console.log(transactioRUB);
-
-// const debets = transactioUSD.filter((el) => el > 0);
-// console.log(debets);
-
-// const credit = transactioUSD.filter((el) => el < 0);
-// console.log(credit);
-
-// const debetsRUB = transactioUSD
-//   .filter((el) => el > 0)
-//   .map((el) => el * courceRUB);
-// console.log(debetsRUB);
-
-// const creditsRUB = transactioUSD
-//   .filter((el) => el < 0)
-//   .map((el) => el * courceRUB);
-// console.log(creditsRUB);
-
-//--------------- Упражнение----------------
-// const prices = [
-//   [100, 200],
-//   [120, 100],
-//   [200, 350],
-//   [135, 160],
+// const users = [
+//   { name: "Вася", age: 30 },
+//   { name: "Катя", age: 18 },
+//   { name: "Аня", age: 40 },
+//   { name: "Петя", age: 25 },
 // ];
 
-// const positivDelta = prices.map((el) => el[1] - el[0]).filter((el) => el > 0);
-
-// console.log(positivDelta);
-
-// const operations = [100, -150, -15, 250, 30, -150, -40];
-// console.log(operations);
-
-// const balance = operations.reduce((acc, value) => (acc += value), 0);
-// console.log(balance);
-
-// const arr = [2, 17, 4, 10];
-
-// const average = arr.reduce((acc, value, i) => {
-//   return i != arr.length - 1 ? (acc += value) : (acc += value) / arr.length;
-// }, 0);
-
-// console.log(average);
-
-// const elG5 = arr.find((value) => value > 5);
-// console.log(elG5);
-
-// const indG5 = arr.findIndex((value) => value < 0);
-// console.log(indG5);
-
-// const arr = [2, 4, 4, 10, 20];
-
-// function some(arr, num) {
-//   const res = arr.find((el) => el === num);
-//   return res != undefined;
-// }
-// console.log(some(arr, 20));
-
-// console.log(arr.some((el) => el === 5));
-
-// const prices = [
-//   [100, 200],
-//   [120, 100],
-//   [200, 350],
-//   [135, [160, 120]],
+// const users = [
+//   {
+//     name: "Вася",
+//     surname: "Пупкин",
+//     age: 30,
+//     skills: ["Разработка", "DevOps"],
+//   },
+//   {
+//     name: "Катя",
+//     surname: "Белова",
+//     age: 18,
+//     skills: ["Дизайн"],
+//   },
 // ];
 
-// const res = prices.flat(2);
-// console.log(res);
+// console.log(users);
 
-//const users = ["Вася", "Семен", "Маша", "Николай", "Alisa"];
-//console.log(users);
-//
-//const res = users.sort();
-//
-//console.log(res);
+// const usersNew = users.map((value) => {
+//   return {
+//     fullName: `${value.name} ${value.surname}`,
+//     skillNum: value.skills.length,
+//   };
+// });
+//console.log(usersNew);
 
-const operations = [100, -300, -100, 50, 450];
-console.log(operations);
-operations.sort((a, b) => b - a);
-console.log(operations);
+// const newObj = users.map((value) => {
+//   fullName: `${value.name} ${value.surname}`;
+// });
+// console.log(newObj);// Ошибка так нельзя
 
-console.log(new Array(5).fill(10).map((el) => Math.random() * 100));
+const user = {
+  name: "Вася",
+  surname: "Пупкин",
+  age: 24,
+  getFullName() {
+    return this.name + " " + this.surname;
+  },
+};
+
+console.log(user.getFullName());
