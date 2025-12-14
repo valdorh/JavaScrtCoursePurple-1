08de8cf49d1f0fbf70d6445c6b382dc0f4a6ae56
Vladimir <doroninva@gmail.com>
2025-12-15 00:15:47 +0300

    Oбъекты конец

--- app.js
+++ app.js
@@ -1,47 +1,83 @@
-// const users = [
-//   { name: "Вася", age: 30 },
-//   { name: "Катя", age: 18 },
-//   { name: "Аня", age: 40 },
-//   { name: "Петя", age: 25 },
-// ];
-
-// const users = [
-//   {
-//     name: "Вася",
-//     surname: "Пупкин",
-//     age: 30,
-//     skills: ["Разработка", "DevOps"],
+// const user = {
+//   name: "Вася",
+//   surname: "Пупкин",
+//   age: 24,
+//   getFullName() {
+//     return this.name + " " + this.surname;
 //   },
-//   {
-//     name: "Катя",
-//     surname: "Белова",
-//     age: 18,
-//     skills: ["Дизайн"],
+// };
+
+// console.log(user.getFullName());
+
+// const wallet = {
+//   ballance: 0,
+//   operations: [],
+//   operation(reason, sum) {
+//     this.ballance += sum;
+//     this.operations.push({ reason: reason, sum: sum });
+//     return this.ballance >= 0 ? true : false;
 //   },
-// ];
+//   countOperations() {
+//     return this.operations.length;
+//   },
+// };
 
-// console.log(users);
+// const wallet = {
+//   ballance: 0,
+//   operations: [],
+//   increase(reason, sum) {
+//     this.ballance += sum;
+//     this.operations.push({ reason, sum });
+//     return true;
+//   },
+//   decrease(reason, sum) {
+//     if (this.ballance < sum) {
+//       console.log("Недостаточно средств");
+//       return false;
+//     } else {
+//       this.ballance -= sum;
+//       this.operations.push({ reason, sum: -sum });
+//       return true;
+//     }
+//   },
+//   countOperations() {
+//     return this.operations.length;
+//   },
+// };
 
-// const usersNew = users.map((value) => {
-//   return {
-//     fullName: `${value.name} ${value.surname}`,
-//     skillNum: value.skills.length,
-//   };
-// });
-//console.log(usersNew);
+// console.log(wallet.increase("Зарплата", 200));
+// console.log(wallet.decrease("Налоги", 200));
+// console.log(wallet.increase("Премия", 100));
+// console.log(wallet.decrease("Купил Iphone", 400));
 
-// const newObj = users.map((value) => {
-//   fullName: `${value.name} ${value.surname}`;
-// });
-// console.log(newObj);// Ошибка так нельзя
+// console.log(wallet.countOperations());
+// console.log(wallet);
 
-const user = {
-  name: "Вася",
-  surname: "Пупкин",
-  age: 24,
-  getFullName() {
-    return this.name + " " + this.surname;
+// const cities = {
+//   msk: {
+//     lt: 150,
+//     temp: 25,
+//   },
+//   spb: {
+//     lt: 200,
+//     temp: 20,
+//   },
+// };
+
+// for (const key in cities) {
+//   console.log(key);
+// }
+
+const cities = {
+  msk: {
+    lt: 150,
+    temp: {
+      celsius: 25,
+    },
+  },
+  spb: {
+    lt: 200,
   },
 };
 
-console.log(user.getFullName());
+console.log(cities.spb?.temp?.celsius);
