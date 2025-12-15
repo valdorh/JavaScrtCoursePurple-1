ac585394a81e24694ca7117c21b92a9edb8d6c38
Vladimir <doroninva@gmail.com>
2025-12-15 11:01:55 +0300

    Объекты конец Склад

--- app.js
+++ app.js
@@ -1,83 +1,56 @@
-// const user = {
-//   name: "Вася",
-//   surname: "Пупкин",
-//   age: 24,
-//   getFullName() {
-//     return this.name + " " + this.surname;
-//   },
-// };
-
-// console.log(user.getFullName());
-
-// const wallet = {
-//   ballance: 0,
-//   operations: [],
-//   operation(reason, sum) {
-//     this.ballance += sum;
-//     this.operations.push({ reason: reason, sum: sum });
-//     return this.ballance >= 0 ? true : false;
-//   },
-//   countOperations() {
-//     return this.operations.length;
-//   },
-// };
-
-// const wallet = {
-//   ballance: 0,
-//   operations: [],
-//   increase(reason, sum) {
-//     this.ballance += sum;
-//     this.operations.push({ reason, sum });
-//     return true;
-//   },
-//   decrease(reason, sum) {
-//     if (this.ballance < sum) {
-//       console.log("Недостаточно средств");
-//       return false;
-//     } else {
-//       this.ballance -= sum;
-//       this.operations.push({ reason, sum: -sum });
-//       return true;
-//     }
-//   },
-//   countOperations() {
-//     return this.operations.length;
-//   },
-// };
-
-// console.log(wallet.increase("Зарплата", 200));
-// console.log(wallet.decrease("Налоги", 200));
-// console.log(wallet.increase("Премия", 100));
-// console.log(wallet.decrease("Купил Iphone", 400));
-
-// console.log(wallet.countOperations());
-// console.log(wallet);
-
-// const cities = {
-//   msk: {
-//     lt: 150,
-//     temp: 25,
-//   },
-//   spb: {
-//     lt: 200,
-//     temp: 20,
-//   },
-// };
-
-// for (const key in cities) {
-//   console.log(key);
-// }
-
-const cities = {
-  msk: {
-    lt: 150,
-    temp: {
-      celsius: 25,
-    },
+const warehouse = {
+  goods: [],
+  findGoodById(id) {
+    return this.goods.find((good) => good.id == id);
   },
-  spb: {
-    lt: 200,
+  addGood(good) {
+    if (good.id && !this.findGoodById(good.id)) {
+      this.goods.push(good);
+    }
+  },
+  getWeight() {
+    return this.goods.reduce(
+      (acc, el) => (acc += el.weight?.kg ? el.weight.kg : 0),
+      0
+    );
   },
 };
 
-console.log(cities.spb?.temp?.celsius);
+const car = {
+  id: 1,
+  weight: {
+    kg: 1000,
+  },
+  brand: "Ford",
+};
+
+const chair = {
+  id: 2,
+  weight: {
+    kg: 2,
+  },
+};
+
+const paper = {
+  id: 3,
+  color: "red",
+};
+
+const musik = {
+  id: 4,
+  weight: {
+    kg: 15,
+  },
+  brand: "Sony",
+};
+
+warehouse.addGood(car);
+warehouse.addGood(chair);
+warehouse.addGood(paper);
+warehouse.addGood(musik);
+console.log(warehouse.findGoodById(2));
+
+console.log(warehouse);
+console.log(warehouse.getWeight());
+
+gods = [1, 2, 3, 4, 5];
