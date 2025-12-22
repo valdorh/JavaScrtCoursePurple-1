37d91993b7275358de2a686a99618667d113aa3d
Vladimir <doroninva@gmail.com>
2025-12-22 09:08:52 +0300

    сохранение данных

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,25 @@
 "use strict";
+
+let habbits = [];
+const HABBIT_KEY = "HABBIT_KEY";
+
+/* utils */
+
+function loadData() {
+  const habbitString = localStorage.getItem("HABBIT_KEY");
+  const habbitArray = JSON.parse(habbitString);
+  if (Array.isArray(habbitArray)) {
+    habbits = habbitArray;
+  }
+}
+
+function saveData() {
+  const habbitString = JSON.stringify(habbits);
+  localStorage.setItem(HABBIT_KEY, habbitString);
+}
+
+/* main code */
+(() => {
+  loadData();
+  console.log(habbits);
+})();
